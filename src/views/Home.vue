1d5db64edd376c6eca34a0760a10040ce00344e1
kremazar <kmazar222@gmail.com>
2020-08-20 21:26:13 +0200

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="home">
     <div class="jumbotron">
-  <h1 class="display-4">Dobrodošli u sustav OnlineFax!</h1>
-  <hr class="my-4">
-  <p>Pristupite fakultetu kojeg pohađate!</p>
-  <p class="lead">
-   <router-link v-bind:to="'/fax/'" class="btn btn-primary btn-lg"> FIPU</router-link>
-  </p>
-</div>
+      <h1 class="display-4">Dobrodošli u sustav OnlineFax!</h1>
+      <hr class="my-4" />
+      <div v-if="login !=undefinded">
+        <p>Pristupite fakultetu kojeg pohađate!</p>
+        <p class="lead">
+          <router-link v-bind:to="'/fax/'" class="btn btn-primary btn-lg">FIPU</router-link>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-import firebase from 'firebase'
+import firebase from "firebase";
+import db from "firebase";
 export default {
-  name:'home',
-  data(){
-    return{
-      
-    }
-  }
-}
+  name: "home",
+  data() {
+    return {
+      login: firebase.auth().currentUser,
+    };
+  },
+};
 </script>
 
 
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,37 +1,35 @@
 <template>
   <div class="about">
-    <div v-if="error" class="list-group-item-danger">
-      {{error}}
-    </div>
+    <div v-if="error" class="list-group-item-danger">{{error}}</div>
     <h1>This is an SignUp page</h1>
-    <div class="container ">
+    <div class="container">
       <div class="row">
-        <div class="col-sm-2">
-
-        </div>
+        <div class="col-sm-2"></div>
         <div class="col-sm-9">
-          <form >
-            <div class="form-group">
-              <label for="FullName">FullName:</label>
-              <input type="FullName" class="form-control"  v-model="user.FullName" placeholder="Enter FullName" />
-            </div>
-            <div class="form-group">
-              <label for="userName">UserName:</label>
-              <input type="userName" class="form-control" placeholder="UserName" v-model="user.UserName" />
-            </div>
+          <form>
             <div class="form-group">
               <label for="Email">Email:</label>
               <input type="Email" class="form-control" v-model="user.email" placeholder="Email" />
             </div>
             <div class="form-group">
               <label for="password">Password:</label>
-              <input type="password" class="form-control" v-model="user.password" placeholder="Password" />
+              <input
+                type="password"
+                class="form-control"
+                v-model="user.password"
+                placeholder="Password"
+              />
             </div>
             <div class="form-group">
               <label for="password2">Confirm Password:</label>
-              <input type="password" class="form-control" v-model="user.password2" placeholder="Confirm Password" />
+              <input
+                type="password"
+                class="form-control"
+                v-model="user.password2"
+                placeholder="Confirm Password"
+              />
             </div>
-            <button v-on:click.prevent="post"  class="btn btn-primary">Submit</button>
+            <button v-on:click.prevent="post" class="btn btn-primary">Submit</button>
           </form>
         </div>
         <div class="col-sm-2"></div>
@@ -41,36 +39,36 @@
 </template>
 
 <script>
-import firebase from 'firebase'
-import db from 'firebase'
-import "firebase/auth"
+import firebase from "firebase";
+import db from "firebase";
+import "firebase/auth";
 export default {
-  name:'signup',
-  data(){
-    
-    return{
-      error:'',
-      ime:'',
-      user:{
-      email:'',
-      password:'',
-      password2:'',
-      
-      FullName:'',
-      UserName:''
-    }
-    }
+  name: "signup",
+  data() {
+    return {
+      error: "",
+      ime: "",
+      user: {
+        email: "",
+        password: "",
+        password2: "",
+
+        FullName: "",
+        UserName: "",
+      },
+    };
   },
-  methods:{
-    post:function(){
+  methods: {
+    post: function () {
       if (this.user.password !== this.user.password2) {
-        this.error ='Password do not match!!!';
-      }else{
-        var db = firebase.firestore()
-      firebase.auth()
-        .createUserWithEmailAndPassword(this.user.email, this.user.password)
-        .then(() => {
-          // postavi podatke o korisniku
+        this.error = "Password do not match!!!";
+      } else {
+        var db = firebase.firestore();
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(this.user.email, this.user.password)
+          .then(() => {
+            /*  // postavi podatke o korisniku
           let id = this.user.email;
           // sada moramo spremiti te dodatne podatke
           db.collection("korisnici")
@@ -83,14 +81,13 @@ export default {
             })
             .catch(function(error) {
               console.error("Error writing document: ", error);
-            });
-        })
-        .then(()=>{
-             this.$router.go('/')
-        })
+            }); */
+          })
+          .then(() => {
+            this.$router.go("/");
+          });
       }
-      
-    }
-  }
-}
+    },
+  },
+};
 </script>
